81c6eb68f77409d7b504a80ad9245ae325ed184c
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2026-07-22 13:52:15 +0300

    Move include "CPP/Windows/Defs.h" out of scope !defined(_WIN32)
    Fixing building on Windows platform.

--- src/HelperFuncs.cpp
+++ src/HelperFuncs.cpp
@@ -9,12 +9,12 @@
 
 #if !defined(_WIN32) && !defined(_OS2)
 #include "CPP/Common/StdAfx.h"
-#include "CPP/Windows/Defs.h"
 #include "CPP/7zip/MyVersion.h"
 #endif
 
 #include "C/7zVersion.h"
 #include "CPP/7zip/Archive/IArchive.h"
+#include "CPP/Windows/Defs.h"
 #include "CPP/Windows/PropVariant.h"
 #include "CPP/Common/MyCom.h"
 #include "CPP/7zip/ICoder.h"
